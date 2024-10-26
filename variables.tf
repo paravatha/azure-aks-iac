edcf62f497248751a91f45f73908067e6089e094
paravatha <prasad.paravatha@jhu.edu>
2024-10-25 23:47:09 -0400

    update for dev

--- variables.tf
+++ variables.tf
@@ -1,16 +1,47 @@
 variable "location" {
-  default = "westus"
+  default = "eastus"
 }
-
 variable "prefix" {
   description = "A prefix used for all resources in this example"
   default     = "mlops-test"
 }
 
-variable "resource_group_name" {
-  default = "aa-playground-sandbox"
+variable "node_count" {
+  description = "The number of nodes in the Kubernetes Node Pool"
+  default     = 2
 }
 
+variable "node_size" {
+  description = "The size of nodes in the Kubernetes Node Pool"
+  default     = "Standard_D2s_v3"
+}
+
+variable "disk_size" {
+  description = "The size of the disk in the Kubernetes Node Pool"
+  default     = 30
+}
+
+variable "tenant_id" {
+  description = "The Tenant ID for the Azure subscription"
+  default     = "ttt"
+}
+
+variable "subscription_id" {
+  description = "The Subscription ID for the Azure subscription"
+  default     = "sss"
+}
+
+variable "resource_group_name" {
+  default = "rg"
+}
+
+variable "client_id" {
+  default = "ccc"
+}
+
+variable "client_secret" {
+  default = "ccc"
+}
 variable "tags" {
   type = map(string)
   default = {
